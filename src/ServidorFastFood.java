da5e313645098bfb0cfaa4d725589dd4abd3f593
LUAN BRENDON <luan_brendonmacedo@hotmail.com>
2023-05-14 00:23:50 -0300

    adicionando comentarios para facilitar a manutencao do cod.

--- src/ServidorFastFood.java
+++ src/ServidorFastFood.java
@@ -1,21 +1,30 @@
 import java.net.MalformedURLException;
-import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
+
 
 public class ServidorFastFood {
 
     public static void main(String[] args){
         try {
+            //Este e o nome pelo qual o servico sera localizado
             String nomeServico = "controlePedido";
 
+            //Cria o objeto a ser servido
             ServicoFastFood servico = new ServicoFastFoodImpl();
+
+            /*Faz a associação do objeto servico com o nome nomeServico
+             na URL rmi://localhost/ usando o método Naming.rebind().
+             */
             Naming.rebind("rmi://localhost/" +nomeServico, servico);
+
+            //Imprime uma mensagem informando que o servidor está no ar e o nome do serviço associado.
             System.out.println("Servidor no ar." + nomeServico);
 
+        /*Trata exceções que podem ocorrer durante a execução do programa.
+        Se uma exceção RemoteException for lançada, uma mensagem de erro será exibida.
+        Se uma exceção MalformedURLException for lançada, uma exceção RuntimeException será lançada.
+         */
         }catch (RemoteException re) {
             System.out.println("Excecao: " +re.getMessage());
         }catch (MalformedURLException e) {
